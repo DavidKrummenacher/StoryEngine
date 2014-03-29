da7c9d0c443987370f48b5e83ac1dd26286e85cf
DaveBentfield <dave@timbookedtwo.com>
2014-03-29 10:47:27 +0100

    Fixed dummy data
    - Fixed dummy data to match constraints

--- _database/dummydata.sql
+++ _database/dummydata.sql
@@ -1,12 +1,4 @@
 --
--- Daten für Tabelle `story_options`
---
-
-INSERT INTO `story_options` (`id`, `source_page`, `order`, `icon`, `text`) VALUES
-(1, 1, 1, 1, 'Go to page 2'),
-(2, 1, 2, 1, 'Go to page 3');
-
---
 -- Daten für Tabelle `story_pages`
 --
 
@@ -14,3 +6,19 @@ INSERT INTO `story_pages` (`id`, `title`, `description`, `content`) VALUES
 (1, 'Test', NULL, 'Blah Blah blah'),
 (2, 'Test 2', NULL, 'Blah blah 2'),
 (3, 'Test 3', NULL, 'Blah blah 3');
+
+--
+-- Daten für Tabelle `story_options`
+--
+
+INSERT INTO `story_options` (`id`, `source_page`, `order`, `icon`, `text`) VALUES
+(1, 1, 1, null, 'Go to page 2'),
+(2, 1, 2, null, 'Go to page 3');
+
+--
+-- Daten für Tabelle `story_option_targets`
+--
+
+INSERT INTO `story_option_targets` (`id`, `option`, `target_page`, `fail`) VALUES
+(1, 1, 2, 0),
+(2, 2, 3, 0);
