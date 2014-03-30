0a552a8d984d123a6e3a80cc4eb99207faef0588
DaveBentfield <dave@timbookedtwo.com>
2014-03-30 14:19:50 +0200

    Added dummy options functionality
    - Added necessairy methods to options model
    - Added dummy views
    - Added dummy controller methods

--- application/controllers/option.php
+++ application/controllers/option.php
@@ -44,6 +44,101 @@ class Option extends CI_Controller {
 		// Redirect to page
 		redirect('page/show/'.$target['target_page']);
 	}
+
+	public function add() {
+		if (!$this->ion_auth->logged_in()) { redirect('admin/login', 'refresh'); }
+		
+		//validate form input
+		/*$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('value', 'Default value', 'required');
+		
+		if (isset($_POST) && !empty($_POST) && $this->form_validation->run() == true) {
+			$name = $this->input->post('name');
+			$description = $this->input->post('description');
+			$value = $this->input->post('value');
+			
+			$this->attributes_model->create($name, $description, $value);
+			redirect('attribute');
+		} else {
+			//set the flash data error message if there is one
+			$this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
+			
+			$this->data['name'] = array(
+				'name'  => 'name',
+				'id'    => 'name',
+				'type'  => 'text',
+				'value' => $this->form_validation->set_value('name'),
+			);
+			$this->data['description'] = array(
+				'name'  => 'description',
+				'id'    => 'description',
+				'type'  => 'text',
+				'value' => $this->input->post('description'),
+			);
+			$this->data['value'] = array(
+				'name'  => 'value',
+				'id'    => 'value',
+				'type'  => 'text',
+				'value' => $this->form_validation->set_value('value'),
+			);
+			
+			$this->_render_page('options/add', $this->data);
+		}*/
+	}
+	
+	public function edit($id) {
+		if (!$this->ion_auth->logged_in()) { redirect('admin/login', 'refresh'); }
+		
+		//validate form input
+		/*$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('value', 'Default value', 'required');
+		
+		if (isset($_POST) && !empty($_POST) && $this->form_validation->run() == true) {
+			$name = $this->input->post('name');
+			$description = $this->input->post('description');
+			$value = $this->input->post('value');
+			
+			$this->attributes_model->update($id, $name, $description, $value);
+			redirect('attribute');
+		} else {
+			//set the flash data error message if there is one
+			$this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
+			
+			$attribute = $this->attributes_model->get($id);
+			$this->data['attribute'] = $attribute;
+			
+			$this->data['name'] = array(
+				'name'  => 'name',
+				'id'    => 'name',
+				'type'  => 'text',
+				'value' => ($this->input->post('name')) ? $this->input->post('name') : $attribute['name'],
+			);
+			$this->data['description'] = array(
+				'name'  => 'description',
+				'id'    => 'description',
+				'type'  => 'text',
+				'value' => ($this->input->post('description')) ? $this->input->post('description') : $attribute['description'],
+			);
+			$this->data['value'] = array(
+				'name'  => 'value',
+				'id'    => 'value',
+				'type'  => 'text',
+				'value' => ($this->input->post('value')) ? $this->input->post('value') : $attribute['value'],
+			);
+			
+			$this->_render_page('options/edit', $this->data);
+		}*/
+	}
+	
+	public function delete($id) {
+		if (!$this->ion_auth->logged_in()) { redirect('admin/login', 'refresh'); }
+		
+		$option = $this->options_model->get($id);
+		$page = $option['source_page'];
+		
+		$this->options_model->delete($id);
+		redirect('edit/page/'.$page);
+	}
 	
 	function _render_page($view, $data = null, $render = false) {
 		$this->viewdata = (empty($data)) ? $this->data: $data;
--- application/models/options_model.php
+++ application/models/options_model.php
@@ -17,23 +17,164 @@ class Options_model extends CI_Model {
 		return $query->result_array();
 	}
 	
-	public function get_conditions_for_option($id) {
-		$query = $this->db->get_where('story_option_conditions', array('option' => $id));
+	public function get($id) {
+		$query = $this->db->get_where('story_options', array('id' => $id));
+		return $query->row_array();
+	}
+	public function create($page, $order, $icon, $text) {
+		$data = array(
+			'source_page' => $page,
+			'order' => $order,
+			'icon' => $icon,
+			'text' => $text
+		);
+		
+		$this->db->insert('story_options', $data);
+	}
+	public function update($id, $page, $order, $icon, $text) {
+		$data = array(
+			'source_page' => $page,
+			'order' => $order,
+			'icon' => $icon,
+			'text' => $text
+		);
+		
+		$this->db->where('id', $id);
+		$this->db->update('story_options', $data);
+	}
+	public function delete($id) {
+		$this->db->delete('story_options', array('id' => $id));
+	}
+	
+	public function get_condition($id) {
+		$query = $this->db->get_where('story_option_conditions', array('id' => $id));
+		return $query->row_array();
+	}
+	public function create_condition($option, $attribute, $comparison, $value) {
+		$data = array(
+			'option' => $option,
+			'attribute' => $attribute,
+			'comparison' => $comparison,
+			'value' => $value
+		);
+		
+		$this->db->insert('story_option_conditions', $data);
+	}
+	public function update_condition($id, $option, $attribute, $comparison, $value) {
+		$data = array(
+			'option' => $option,
+			'attribute' => $attribute,
+			'comparison' => $comparison,
+			'value' => $value
+		);
+		
+		$this->db->where('id', $id);
+		$this->db->update('story_option_conditions', $data);
+	}
+	public function delete_condition($id) {
+		$this->db->delete('story_option_conditions', array('id' => $id));
+	}
+	public function get_conditions_for_option($option) {
+		$query = $this->db->get_where('story_option_conditions', array('option' => $option));
 		return $query->result_array();
 	}
 	
-	public function get_targets_for_option($id, $fail) {
-		$query = $this->db->get_where('story_option_targets', array('option' => $id, 'fail' => $fail));
+	public function get_target($id) {
+		$query = $this->db->get_where('story_option_targets', array('id' => $id));
+		return $query->row_array();
+	}
+	public function create_target($option, $target_page, $fail) {
+		$data = array(
+			'option' => $option,
+			'target_page' => $target_page,
+			'fail' => $fail
+		);
+		
+		$this->db->insert('story_option_targets', $data);
+	}
+	public function update_target($id, $option, $target_page, $fail) {
+		$data = array(
+			'option' => $option,
+			'target_page' => $target_page,
+			'fail' => $fail
+		);
+		
+		$this->db->where('id', $id);
+		$this->db->update('story_option_targets', $data);
+	}
+	public function delete_target($id) {
+		$this->db->delete('story_option_targets', array('id' => $id));
+	}
+	public function get_targets_for_option($option, $fail) {
+		$query = $this->db->get_where('story_option_targets', array('option' => $option, 'fail' => $fail));
 		return $query->result_array();
 	}
 	
-	public function get_checks_for_option($id) {
-		$query = $this->db->get_where('story_option_checks', array('option' => $id));
+	public function get_check($id) {
+		$query = $this->db->get_where('story_option_checks', array('id' => $id));
+		return $query->row_array();
+	}
+	public function create_check($option, $attribute, $comparison, $value, $random) {
+		$data = array(
+			'option' => $option,
+			'attribute' => $attribute,
+			'comparison' => $comparison,
+			'value' => $value,
+			'random' => $random
+		);
+		
+		$this->db->insert('story_option_checks', $data);
+	}
+	public function update_check($id, $option, $attribute, $comparison, $value, $random) {
+		$data = array(
+			'option' => $option,
+			'attribute' => $attribute,
+			'comparison' => $comparison,
+			'value' => $value,
+			'random' => $random
+		);
+		
+		$this->db->where('id', $id);
+		$this->db->update('story_option_checks', $data);
+	}
+	public function delete_check($id) {
+		$this->db->delete('story_option_checks', array('id' => $id));
+	}
+	public function get_checks_for_option($option) {
+		$query = $this->db->get_where('story_option_checks', array('option' => $option));
 		return $query->result_array();
 	}
 	
-	public function get_consequences_for_option($id) {
-		$query = $this->db->get_where('story_option_consequences', array('option' => $id));
+	public function get_consequence($id) {
+		$query = $this->db->get_where('story_option_consequences', array('id' => $id));
+		return $query->row_array();
+	}
+	public function create_consequence($option, $attribute, $operator, $value) {
+		$data = array(
+			'option' => $option,
+			'attribute' => $attribute,
+			'operator' => $operator,
+			'value' => $value
+		);
+		
+		$this->db->insert('story_option_consequences', $data);
+	}
+	public function update_consequence($id, $option, $attribute, $operator, $value) {
+		$data = array(
+			'option' => $option,
+			'attribute' => $attribute,
+			'operator' => $operator,
+			'value' => $value
+		);
+		
+		$this->db->where('id', $id);
+		$this->db->update('story_option_consequences', $data);
+	}
+	public function delete_consequence($id) {
+		$this->db->delete('story_option_consequences', array('id' => $id));
+	}
+	public function get_consequences_for_option($option) {
+		$query = $this->db->get_where('story_option_consequences', array('option' => $option));
 		return $query->result_array();
 	}
 	
