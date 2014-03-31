bd938731dd6ad7f847794fd903ba0bc2823e6dea
DaveBentfield <dave@timbookedtwo.com>
2014-03-31 08:48:01 +0200

    Added tabs to option edit view
    - Added tabs
    - Moved modals to bottom of the page

--- application/views/options/edit.php
+++ application/views/options/edit.php
@@ -35,28 +35,106 @@
 	</div>
 <?php echo form_close();?>
 
-<h2>Conditions</h2>
-<table class="table table-condensed">
-	<tr>
-		<th>Condition</th>
-		<th width="80px"><?php echo lang('index_action_th');?></th>
-	</tr>
-	<?php foreach ($conditions as $condition):?>
-		<tr>
-			<td><?php echo $condition['id'];?></td>
-			<td>
-				<div class="btn-group">
-					<?php
-						echo anchor("option/edit_condition/".$condition['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
-						echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteConditionModal'.$condition['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
-					?>
-				</div>
-			</td>
-		</tr>
-	<?php endforeach;?>
-</table>
+<ul class="nav nav-tabs">
+	<li class="active"><a href="#conditions" data-toggle="tab">Conditions</a></li>
+	<li><a href="#targets" data-toggle="tab">Targets</a></li>
+	<li><a href="#checks" data-toggle="tab">Checks</a></li>
+	<li><a href="#consequences" data-toggle="tab">Consequences</a></li>
+</ul>
 
-<p><?php echo anchor('option/add_condition/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add condition', 'class="btn btn-default"'); ?></p>
+<div class="tab-content">
+	<div class="tab-pane active" id="conditions">
+		<table class="table table-condensed">
+			<tr>
+				<th>Condition</th>
+				<th width="80px"><?php echo lang('index_action_th');?></th>
+			</tr>
+			<?php foreach ($conditions as $condition):?>
+				<tr>
+					<td><?php echo $condition['id'];?></td>
+					<td>
+						<div class="btn-group">
+							<?php
+								echo anchor("option/edit_condition/".$condition['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
+								echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteConditionModal'.$condition['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
+							?>
+						</div>
+					</td>
+				</tr>
+			<?php endforeach;?>
+		</table>
+		<p><?php echo anchor('option/add_condition/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add condition', 'class="btn btn-default"'); ?></p>
+	</div>
+	
+	<div class="tab-pane" id="targets">
+		<table class="table table-condensed">
+			<tr>
+				<th>Target</th>
+				<th width="80px"><?php echo lang('index_action_th');?></th>
+			</tr>
+			<?php foreach ($targets as $target):?>
+				<tr>
+					<td><?php echo $target['target_page'];?></td>
+					<td>
+						<div class="btn-group">
+							<?php
+								echo anchor("option/edit_target/".$target['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
+								echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteTargetModal'.$target['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
+							?>
+						</div>
+					</td>
+				</tr>
+			<?php endforeach;?>
+		</table>
+		<p><?php echo anchor('option/add_target/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add target', 'class="btn btn-default"'); ?></p>
+	</div>
+	
+	<div class="tab-pane" id="checks">
+		<table class="table table-condensed">
+			<tr>
+				<th>Check</th>
+				<th width="80px"><?php echo lang('index_action_th');?></th>
+			</tr>
+			<?php foreach ($checks as $check):?>
+				<tr>
+					<td><?php echo $check['id'];?></td>
+					<td>
+						<div class="btn-group">
+							<?php
+								echo anchor("option/edit_check/".$check['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
+								echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteCheckModal'.$check['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
+							?>
+						</div>
+					</td>
+				</tr>
+			<?php endforeach;?>
+		</table>
+		<p><?php echo anchor('option/add_check/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add check', 'class="btn btn-default"'); ?></p>
+	</div>
+	
+	<div class="tab-pane" id="consequences">
+		<table class="table table-condensed">
+			<tr>
+				<th>Consequence</th>
+				<th width="80px"><?php echo lang('index_action_th');?></th>
+			</tr>
+			<?php foreach ($consequences as $consequence):?>
+				<tr>
+					<td><?php echo $consequence['id'];?></td>
+					<td>
+						<div class="btn-group">
+							<?php
+								echo anchor("option/edit_consequence/".$consequence['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
+								echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteConsequenceModal'.$consequence['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
+							?>
+						</div>
+					</td>
+				</tr>
+			<?php endforeach;?>
+		</table>
+		<p><?php echo anchor('option/add_consequence/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add consequence', 'class="btn btn-default"'); ?></p>
+	</div>
+</div>
 
 <?php foreach ($conditions as $condition):?>
 <div class="modal fade" id="deleteConditionModal<?php echo $condition['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="deleteConditionModal<?php echo $condition['id']; ?>Label" aria-hidden="true">
@@ -80,29 +158,6 @@
 </div>
 <?php endforeach;?>
 
-<h2>Targets</h2>
-<table class="table table-condensed">
-	<tr>
-		<th>Target</th>
-		<th width="80px"><?php echo lang('index_action_th');?></th>
-	</tr>
-	<?php foreach ($targets as $target):?>
-		<tr>
-			<td><?php echo $target['target_page'];?></td>
-			<td>
-				<div class="btn-group">
-					<?php
-						echo anchor("option/edit_target/".$target['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
-						echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteTargetModal'.$target['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
-					?>
-				</div>
-			</td>
-		</tr>
-	<?php endforeach;?>
-</table>
-
-<p><?php echo anchor('option/add_target/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add target', 'class="btn btn-default"'); ?></p>
-
 <?php foreach ($targets as $target):?>
 <div class="modal fade" id="deleteTargetModal<?php echo $target['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="deleteTargetModal<?php echo $target['id']; ?>Label" aria-hidden="true">
 	<div class="modal-dialog">
@@ -125,29 +180,6 @@
 </div>
 <?php endforeach;?>
 
-<h2>Checks</h2>
-<table class="table table-condensed">
-	<tr>
-		<th>Check</th>
-		<th width="80px"><?php echo lang('index_action_th');?></th>
-	</tr>
-	<?php foreach ($checks as $check):?>
-		<tr>
-			<td><?php echo $check['id'];?></td>
-			<td>
-				<div class="btn-group">
-					<?php
-						echo anchor("option/edit_check/".$check['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
-						echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteCheckModal'.$check['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
-					?>
-				</div>
-			</td>
-		</tr>
-	<?php endforeach;?>
-</table>
-
-<p><?php echo anchor('option/add_check/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add check', 'class="btn btn-default"'); ?></p>
-
 <?php foreach ($checks as $check):?>
 <div class="modal fade" id="deleteCheckModal<?php echo $check['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="deleteCheckModal<?php echo $check['id']; ?>Label" aria-hidden="true">
 	<div class="modal-dialog">
@@ -169,29 +201,6 @@
 	</div>
 </div>
 <?php endforeach;?>
-
-<h2>Consequences</h2>
-<table class="table table-condensed">
-	<tr>
-		<th>Consequence</th>
-		<th width="80px"><?php echo lang('index_action_th');?></th>
-	</tr>
-	<?php foreach ($consequences as $consequence):?>
-		<tr>
-			<td><?php echo $consequence['id'];?></td>
-			<td>
-				<div class="btn-group">
-					<?php
-						echo anchor("option/edit_consequence/".$consequence['id'], '<span class="glyphicon glyphicon-pencil"></span>', 'class="btn btn-default btn-xs" title="Bearbeiten"');
-						echo '<a class="btn btn-default btn-xs" title="Löschen" data-toggle="modal" href="#deleteConsequenceModal'.$consequence['id'].'"><span class="glyphicon glyphicon-trash"></span></a>';
-					?>
-				</div>
-			</td>
-		</tr>
-	<?php endforeach;?>
-</table>
-
-<p><?php echo anchor('option/add_consequence/'.$option['id'], '<span class="glyphicon glyphicon-plus"></span> Add consequence', 'class="btn btn-default"'); ?></p>
 
 <?php foreach ($consequences as $consequence):?>
 <div class="modal fade" id="deleteConsequenceModal<?php echo $consequence['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="deleteConsequenceModal<?php echo $consequence['id']; ?>Label" aria-hidden="true">
