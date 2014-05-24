84e8eaaa5b93a027a9cad240a0ee683f8516d840
DaveBentfield <dave@timbookedtwo.com>
2014-05-24 11:34:29 +0200

    Added additional class

--- application/views/achievements/show.php
+++ application/views/achievements/show.php
@@ -10,7 +10,7 @@
 	?>
 	<?php foreach ($achievements as $achievement) { ?>
 	<?php $counter++; ?>
-	<div class="col-xs-<?php echo $xs_size ?> col-sm-<?php echo $sm_size ?> col-md-<?php echo $md_size ?> col-lg-<?php echo $lg_size ?>">
+	<div class="achievement-entity col-xs-<?php echo $xs_size ?> col-sm-<?php echo $sm_size ?> col-md-<?php echo $md_size ?> col-lg-<?php echo $lg_size ?>">
 		<img src="<?php echo base_url('assets/achievements/mobile/'.$achievement['mobile_uri']); ?>" class="achievement-image img-responsive"<?php if (!$achievement['unlocked']) { ?> style="opacity:0.4;"<?php } ?> />
 		<h4 class="achievement-title"><?php echo ($achievement['unlocked']) ? $achievement['name'] : '???';?></h4>
 		<p class="achievement-content"><?php echo ($achievement['unlocked']) ? $achievement['description'] : '???';?></p>
